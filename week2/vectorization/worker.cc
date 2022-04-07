e04dd5a56d53ef8457e0477275702ada64fbf773
Jobayer <tmij.cs@gmail.com>
2022-04-07 15:22:30 +0600

    change access pattern in loop

--- week2/vectorization/worker.cc
+++ week2/vectorization/worker.cc
@@ -2,10 +2,9 @@ double MyFunction(int n) {
     double A[n], B[n];
     A[0:n] = B[0:n] = 1.0;
 
-    for (int i = 0; i < n; i++) {
-        B[i] *= (double)i;
-        A[i] += B[i];
+    for (int i = 0; i < n/2; i++) {
+        A[i] += B[i*2];
     }
-    
+
     return A[2];
 }
