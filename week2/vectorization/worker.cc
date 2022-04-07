4f69ba666c906169cbacbed26bbb16b34a1dfd88
Jobayer <tmij.cs@gmail.com>
2022-04-07 15:36:07 +0600

    addition of exp

--- week2/vectorization/worker.cc
+++ week2/vectorization/worker.cc
@@ -1,9 +1,11 @@
+#include <cmath>
+
 double MyFunction(int n) {
     double A[n], B[n];
     A[0:n] = B[0:n] = 1.0;
 
-    for (int i = 0; i < n/2; i++) {
-        if (i%3 == 0) A[i] += B[i];
+    for (int i = 0; i < n; i++) {
+        A[i] += exp(B[i]);
     }
 
     return A[2];
