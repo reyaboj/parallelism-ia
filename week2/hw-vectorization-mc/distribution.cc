0139fd0bbd497e04870e059e5909348678c6ddfd
Jobayer <tmij.cs@gmail.com>
2022-04-10 15:59:58 +0600

    hint preferred vector length

--- week2/hw-vectorization-mc/distribution.cc
+++ week2/hw-vectorization-mc/distribution.cc
@@ -1,7 +1,7 @@
 #include "distribution.h"
 
 //distribution function definition
-#pragma omp declare simd
+#pragma omp declare simd simdlen(16)
 float dist_func(const float alpha, const float rn) {
   return delta_max*sinf(alpha*rn)*expf(-rn*rn);
 }
