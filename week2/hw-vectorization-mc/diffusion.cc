63d22b367644ec80a3f257227754a8bdf25840a3
Jobayer <tmij.cs@gmail.com>
2022-04-10 14:08:53 +0600

    fix initialization code

--- week2/hw-vectorization-mc/diffusion.cc
+++ week2/hw-vectorization-mc/diffusion.cc
@@ -9,8 +9,14 @@ int diffusion(const int n_particles,
               const float alpha, 
               VSLStreamStatePtr rnStream) {
   int n_escaped=0;
-  float particle_pos[n_particles] {};
-  float rn[n_particles] {};
+  float particle_pos[n_particles];
+  float rn[n_particles];
+
+  // initialize positions and random displacements
+  for (int i = 0; i < n_particles; i++) {
+    particle_pos[i] = 0.0f;
+    rn[i] = 0.0f;
+  }
 
   for (int j = 0; j < n_steps; j++) {
     //Intel MKL function to generate random numbers
